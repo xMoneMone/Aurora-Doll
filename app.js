af91c036d99a3e9e79c477d04d4f1c12b013cda6
Ellie <yokomone@yahoo.com>
2023-04-15 04:08:10 +0300

    added deletion of clothing item functionality

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 let buttons = document.querySelectorAll("button")
 
 let selected = ""
+let selected_directory = ""
+let targeted_doll_item = ""
 
 for (let button of buttons){
     button.addEventListener("click", () => {
-        selected = button
+        selected = button.id.replace("_button", "")
+        selected_directory = "graphics/" + selected
+        targeted_doll_item = document.getElementById("current_" + selected)
         for (let button of buttons){
             button.classList.remove('selected_button')
         }
@@ -12,3 +16,9 @@ for (let button of buttons){
     })
 
 }
+
+document.getElementById("fringes_button").click()
+
+document.getElementById("nothing").addEventListener("click", () => {
+    targeted_doll_item.src = ""
+})
